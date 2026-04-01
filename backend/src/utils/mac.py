457cf4028bf845c574bc1e6214174ad5f27b1942
adabismail <adabismail000@gmail.com>
2026-04-01 22:00:27 +0530

    Removed redundant code

--- backend/src/utils/mac.py
+++ backend/src/utils/mac.py
@@ -1,14 +1,3 @@
-# import random
-# def giveMacAddress(companyIdentifier):
-#     # unique 48 bit mac address each time it is called.
-#     macAddress=companyIdentifier
-#     digits='0123456789ABCDEF'
-#     for i in range(0,3):
-#         idx1=random.randint(0,15)
-#         idx2=random.randint(0,15)
-#         macAddress+=f':{digits[idx1]}{digits[idx2]}'
-#     return macAddress
-
 import random
 
 def giveMacAddress(companyIdentifier):
